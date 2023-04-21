c279c1583742c923910ad48a3a54b158ecc4ab62
polyhistor2050 <chrisspaul2050@gmail.com>
2023-04-21 17:46:07 +0300

    add set of trumplet and flute sound to the kit

--- script.js
+++ script.js
@@ -4,45 +4,56 @@ for (let i = 0; i < btn.length; i++) {
     function handlerClick(){
         let btnValue = this.innerHTML;
         makeSound(btnValue);
+        buttonAnimation(btnValue);
     }
 }
 
 document.addEventListener("keypress", function(e) {
     makeSound(e.key);
+    buttonAnimation(e.key);
 });
 
 
 function makeSound(key) {
     switch(key) {
-        case "w":
-            let tom1 = new Audio("sounds/tom-1.mp3");
+        case "q":
+            let tom1 = new Audio("assets/band/tom-1.mp3");
             tom1.play();
             break;
-        case "a":
-            let tom2 = new Audio("sounds/tom-2.mp3");
+        case "w":
+            let tom2 = new Audio("assets/band/tom-2.mp3");
             tom2.play();
             break;
-        case "s":
-            let tom3 = new Audio("sounds/tom-3.mp3");
+        case "e":
+            let tom3 = new Audio("assets/band/tom-3.mp3");
             tom3.play();
             break;
-        case "d":
-            let tom4 = new Audio("sounds/tom-4.mp3");
+        case "r":
+            let tom4 = new Audio("assets/band/tom-4.mp3");
             tom4.play();
             break;
-        case "j":
-            let snare = new Audio("sounds/snare.mp3");
+        case "t":
+            let snare = new Audio("assets/band/snare.mp3");
             snare.play();
             break;
-        case "k":
-            let crash = new Audio("sounds/crash.mp3");
+        case "y":
+            let crash = new Audio("assets/band/crash.mp3");
             crash.play();
             break;
-        case "l":
-            let kick = new Audio("sounds/kick-bass.mp3");
+        case "u":
+            let kick = new Audio("assets/band/kick-bass.mp3");
             kick.play();
             break;
 
         default: console.log(btnValue);
     }
+}
+
+function buttonAnimation(currentkey) {
+    let activebtn = document.querySelector("." + currentkey);
+    activebtn.classList.add("pressed");
+    setTimeout(function() {
+        activebtn.classList.remove("pressed");
+    }, 100);
+    
 }
