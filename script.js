a0c96695cf539d9eecee5fbb017e586cf95883df
polyhistor2050 <chrisspaul2050@gmail.com>
2023-04-22 16:36:04 +0300

    add functionality to flute and trump kit

--- script.js
+++ script.js
@@ -1,51 +1,129 @@
-let btn = document.querySelectorAll(".drum");
+// Selecting button with class kit
+let btn = document.querySelectorAll(".kit");
+
+// Iterating through button with class kit 
 for (let i = 0; i < btn.length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handlerClick);
+    document.querySelectorAll(".kit")[i].addEventListener("click", handlerClick);
+
+    //EventListener for Mouse click
     function handlerClick(){
         let btnValue = this.innerHTML;
-        makeSound(btnValue);
+        makeDrumSound(btnValue);
+        makeTrumpletSound(btnValue);
+        makeFluteSound(btnValue);
         buttonAnimation(btnValue);
+
     }
 }
-
+//EventListener for Keyboard press
 document.addEventListener("keypress", function(e) {
-    makeSound(e.key);
+    makeDrumSound(e.key);
+    makeTrumpletSound(e.key);
+    makeFluteSound(e.key);
     buttonAnimation(e.key);
 });
 
-
-function makeSound(key) {
+//Drum sounds
+function makeDrumSound(key) {
     switch(key) {
         case "q":
-            let tom1 = new Audio("assets/band/tom-1.mp3");
+            const tom1 = new Audio("assets/band/tom-1.mp3");
             tom1.play();
             break;
         case "w":
-            let tom2 = new Audio("assets/band/tom-2.mp3");
+            const tom2 = new Audio("assets/band/tom-2.mp3");
             tom2.play();
             break;
         case "e":
-            let tom3 = new Audio("assets/band/tom-3.mp3");
+            const tom3 = new Audio("assets/band/tom-3.mp3");
             tom3.play();
             break;
         case "r":
-            let tom4 = new Audio("assets/band/tom-4.mp3");
+            const tom4 = new Audio("assets/band/tom-4.mp3");
             tom4.play();
             break;
         case "t":
-            let snare = new Audio("assets/band/snare.mp3");
+            const snare = new Audio("assets/band/snare.mp3");
             snare.play();
             break;
         case "y":
-            let crash = new Audio("assets/band/crash.mp3");
+            const crash = new Audio("assets/band/crash.mp3");
             crash.play();
             break;
         case "u":
-            let kick = new Audio("assets/band/kick-bass.mp3");
+            const kick = new Audio("assets/band/kick-bass.mp3");
             kick.play();
             break;
 
-        default: console.log(btnValue);
+        // default: console.log(btnValue);
+    }
+}
+
+//Trumple Sounds
+function makeTrumpletSound(key){
+    switch(key){
+        case "a":
+            const fail = new Audio("assets/trumplet/fail-144746.mp3");
+            fail.play();
+            break;
+        case "s":
+            const fanfare = new Audio("assets/trumplet/fanfare-46385.mp3");
+            fanfare.play();
+            break;
+        case "d":
+            const tada = new Audio("assets/trumplet/tada-fanfare-a-6313.mp3");
+            tada.play();
+            break;
+        case "f":
+            const horn = new Audio("assets/trumplet/horn-toy-claun-like-sound-1-94986.mp3");
+            horn.play();
+            break;
+        case "g":
+            const success = new Audio("assets/trumplet/success-fanfare-trumpets-6185.mp3");
+            success.play();
+            break;
+        case "h":
+            const horse = new Audio("assets/trumplet/trumpet-horse-whinney-38590.mp3");
+            horse.play();
+            break;
+        case "j":
+            const funny = new Audio("assets/trumplet/medieval-fanfare-6826.mp3");
+            funny.play();
+            break;
+    }
+}
+
+//Flute Sounds
+function makeFluteSound(key){
+    switch(key){
+        case "z":
+            const cinematic = new Audio("assets/flute/mixkit-cinematic-tribal-flute-2306.wav");
+            cinematic.play();
+            break;
+        case "x":
+            const alert = new Audio("assets/flute/mixkit-flute-alert-2307.wav");
+            alert.play();
+            break;
+        case "c":
+            const cellPhone =  new Audio("assets/flute/mixkit-flute-cell-phone-alert-2315.wav");
+            cellPhone.play();
+            break;
+        case "v":
+            const toyWhistle = new Audio("assets/flute/mixkit-flute-toy-whistle-2812.wav");
+            toyWhistle.play();
+            break;
+        case "b":
+            const game = new Audio("assets/flute/mixkit-game-flute-bonus-2313.wav");
+            game.play();
+            break;
+        case "n":
+            const musicalAlert = new Audio("assets/flute/mixkit-musical-flute-alert-2308.wav");
+            musicalAlert.play();
+            break;
+        case "m":
+            const uplifting = new Audio("assets/flute/mixkit-uplifting-flute-notification-2317.wav");
+            uplifting.play();
+            break;
     }
 }
 
